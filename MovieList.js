fcd2575493c994ff9fdeea0798fe4e53926eded1
Junseo Kim <juny7s6@khu.ac.kr>
2022-06-02 01:35:52 +0900

    Delete main.js part

--- MovieList.js
+++ MovieList.js
@@ -1,39 +1,15 @@
+//=============================================================
 var express = require('express');
 const request = require('request');
-var config = require('./config.json');
-const TARGET_URL = 'https://api.line.me/v2/bot/message/reply'
+const config = require('./config.json');
+//=============================================================
+const LINE_URL = 'https://api.line.me/v2/bot/message/reply'
 const TOKEN = config.TOKEN;
-const KOFIC_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json'
+const BOXOFFICE_URL = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
 const KOFIC_KEY = config.KOFIC_KEY;
+//=============================================================
 
-const fs = require('fs');
-const path = require('path');
-const HTTPS = require('https');
-const domain = config.domain;
-const sslport = 23023;
-const bodyParser = require('body-parser');
-var app = express();
-var translang = 'en';
-app.use(bodyParser.json());
-app.post('/hook', function (req, res) {
-
-    var eventObj = req.body.events[0];
-    var source = eventObj.source;
-    var message = eventObj.message;
-
-    // request log
-    console.log('======================', new Date() ,'======================');
-    console.log('[request]', req.body);
-    console.log('[request source] ', eventObj.source);
-    console.log('[request message]', eventObj.message);
-
-    MovieInfo(eventObj.replyToken, eventObj.message.text);
-    
-
-    res.sendStatus(200);
-});
-
-function MovieInfo(replyToken, message) {
+export function MovieInfo(replyToken, message) {
 
     request.get(
         {
@@ -65,19 +41,3 @@ function MovieInfo(replyToken, message) {
         });
 
 }
-
-try {
-    const option = {
-      ca: fs.readFileSync('/etc/letsencrypt/live/' + domain +'/fullchain.pem'),
-      key: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/privkey.pem'), 'utf8').toString(),
-      cert: fs.readFileSync(path.resolve(process.cwd(), '/etc/letsencrypt/live/' + domain +'/cert.pem'), 'utf8').toString(),
-    };
-  
-    HTTPS.createServer(option, app).listen(sslport, () => {
-      console.log(`[HTTPS] Server is started on port ${sslport}`);
-    });
-  } catch (error) {
-    console.log('[HTTPS] HTTPS 오류가 발생하였습니다. HTTPS 서버는 실행되지 않습니다.');
-    console.log(error);
-  }
-  
